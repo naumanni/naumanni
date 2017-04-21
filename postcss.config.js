0446f5c0c7b0e23f19c5ee59f41b03e278a05e94
Shin ADACHI <shn@glucose.jp>
2017-04-21 19:32:19 +0900

    add boilerplate

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'plugins': [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-nested'),
+    require('postcss-color-function'),
+    require('postcss-simple-vars'),
+    require('postcss-cssnext'),
+  ],
+  'autoprefixer': {
+    'browsers': [
+      'last 2 versions',
+      'iOS >= 9',
+      'Android >= 5.0',
+    ],
+  },
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,75 @@
+/* eslint-disable require-jsdoc */
+import DefinePlugin from 'webpack/lib/DefinePlugin'
+
+
+class LoggerPlugin {
+  apply(compiler) {
+    const timestamp = () => `[${(new Date()).toLocaleString()}]`
+    compiler.plugin('compile', (params) => {
+      console.log('\x1b[1;36m' + '================================')
+      console.log(timestamp() + ' Start compile' + '\x1b[0m')
+    })
+    compiler.plugin('after-emit', (params, callback) => {
+      callback()
+      console.log('\x1b[1;35m' + timestamp() + ' Finish compile')
+      console.log('================================' + '\x1b[0m')
+    })
+  }
+}
+
+
+const SOURCE_DIR = `${__dirname}/src/js`
+const DEST_DIR = `${__dirname}/static`
+
+
+module.exports = {
+  entry: {
+    main: ['babel-polyfill', `${SOURCE_DIR}/main.es6`],
+  },
+  output: {
+    path: DEST_DIR,
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].[chunkhash].chunked.js',
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: /(node_modules)/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+          failOnError: true,
+        },
+        test: /\.es6$/,
+      },
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+        },
+        test: /\.es6$/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.es6', '.js'],
+    modules: [
+      SOURCE_DIR,
+      'node_modules',
+    ],
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new LoggerPlugin(),
+  ],
+}
+
+
+if(process.env.NODE_ENV != 'production') {
+
+}
+
