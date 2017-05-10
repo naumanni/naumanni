f6ffcaa736c085eb6064710fc673435f0809e311
Shin ADACHI <shn@glucose.jp>
2017-05-10 17:03:34 +0900

    React-Storybookでasync/awaitを使えるように変更

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,18 @@
 module.exports = function(storybookBaseConfig, configType) {
+  storybookBaseConfig.entry.preview.unshift('babel-polyfill')
+
   storybookBaseConfig.resolve.extensions.unshift('.es6')
-  storybookBaseConfig.module.loaders.push({
-    test: /\.es6$/,
-    exclude: /node_modules/,
-    loader: 'babel',
-  })
+  storybookBaseConfig.module.loaders.push(
+    {
+      test: /\.es6$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+    },
+    {
+      test: /\.json$/,
+      loader: 'json',
+    }
+  )
 
   return storybookBaseConfig;
 }
